ee926115dd13bcc091774ed285173509a2f8f152
Hoon Lee <yl.hoony@gmail.com>
2018-01-04 19:53:00 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -17,7 +17,7 @@ class App < Sinatra::Base
 
   get '/goodbye/:name' do
     @user_name = params[:name]
-    "Goodbye, #{@user_name}"
+    "Goodbye, #{@user_name}."
   end
 
   get '/multiply/:num1/:num2' do
